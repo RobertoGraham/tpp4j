3a134f90dbe1412b96551252ba6f7842b0732f90
Roberto Graham <roberto.graham@sainsburys.co.uk>
2024-04-08 13:30:07 +0100

    :zap: Switch to signing certificates with Elliptic Curve keys for faster start up.

--- src/main/java/io/github/robertograham/tpp4j/PrivateCredentialHelper.java
+++ src/main/java/io/github/robertograham/tpp4j/PrivateCredentialHelper.java
@@ -3,12 +3,12 @@ package io.github.robertograham.tpp4j;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
-import java.security.SecureRandom;
-import java.security.spec.RSAKeyGenParameterSpec;
 import java.time.Instant;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.sec.SECNamedCurves;
+import org.bouncycastle.asn1.sec.SECObjectIdentifiers;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x509.Extension;
@@ -17,15 +17,15 @@ import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
-import org.bouncycastle.crypto.generators.RSAKeyPairGenerator;
-import org.bouncycastle.crypto.params.RSAKeyGenerationParameters;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+import org.bouncycastle.crypto.params.ECNamedDomainParameters;
 import org.bouncycastle.crypto.util.PrivateKeyInfoFactory;
 import org.bouncycastle.crypto.util.SubjectPublicKeyInfoFactory;
-import org.bouncycastle.jcajce.provider.asymmetric.util.PrimeCertaintyCalculator;
 import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
 import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
 import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.bouncycastle.operator.bc.BcECContentSignerBuilder;
 
 final class PrivateCredentialHelper {
 
@@ -44,9 +44,9 @@ final class PrivateCredentialHelper {
     }
 
     private static AsymmetricCipherKeyPair createKeyPair() throws GeneralSecurityException {
-        final var generator = new RSAKeyPairGenerator();
-        generator.init(new RSAKeyGenerationParameters(RSAKeyGenParameterSpec.F4, new SecureRandom(), 4096,
-                PrimeCertaintyCalculator.getDefaultCertainty(4096)));
+        final var generator = new ECKeyPairGenerator();
+        generator.init(new ECKeyGenerationParameters(
+                new ECNamedDomainParameters(SECObjectIdentifiers.secp384r1, SECNamedCurves.getByOID(SECObjectIdentifiers.secp384r1)), null));
 
         return generator.generateKeyPair();
     }
@@ -59,14 +59,14 @@ final class PrivateCredentialHelper {
         final var serial = BigInteger.valueOf(Instant.now()
                 .toEpochMilli());
         final var now = Instant.now();
-        final var signatureAlgorithmIdentifier = new DefaultSignatureAlgorithmIdentifierFinder().find("SHA256WITHRSA");
+        final var signatureAlgorithmIdentifier = new DefaultSignatureAlgorithmIdentifierFinder().find("SHA384WITHECDSA");
         final var digestAlgorithmIdentifier = new DefaultDigestAlgorithmIdentifierFinder().find(signatureAlgorithmIdentifier);
 
         return new X509v3CertificateBuilder(issuerAndSubject, serial, Date.from(now),
                 Date.from(now.plusSeconds(TimeUnit.DAYS.toSeconds(1L))), issuerAndSubject,
                 SubjectPublicKeyInfoFactory.createSubjectPublicKeyInfo(keyPair.getPublic()))
                 .addExtension(Extension.subjectAlternativeName, false, new DERSequence((new GeneralName(GeneralName.dNSName, "localhost"))))
-                .build(new BcRSAContentSignerBuilder(signatureAlgorithmIdentifier, digestAlgorithmIdentifier)
+                .build(new BcECContentSignerBuilder(signatureAlgorithmIdentifier, digestAlgorithmIdentifier)
                         .build(keyPair.getPrivate()));
     }
 }
